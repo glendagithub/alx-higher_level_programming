b741005414c26a0ec02d0486d14db4b3b40e2278
glendagithub <glandarathakga@gmail.com>
2023-09-06 17:01:37 -0700

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+
+
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+def remove_char_at(str, n):
+#endif
